b8625d47a8342eb2e73b1d6f056aca320abe73fe
viktoriaschuster <viki.schuster@web.de>
2023-06-03 13:25:06 +0200

    add jonas stuff

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div class="w-full p-4">
+    <main
+      role="main"
+      class="w-full flex flex-col h-screen content-center justify-center"
+    >
+      <div class="w-full sm:w-1/2 lg:w-2/3 bg-gray-50 rounded-xl m-auto">
+        <div class="bg-white rounded shadow px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <HelloWorld />
+          <Paper />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Paper from "./components/Paper.vue";
+
+export default {
+  components: {
+    HelloWorld,
+    Paper,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Paper.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card bg-base-100 shadow-xl">
+    <figure>
+      <img
+        src="../assets/IMG_1467_edited copy.jpeg"
+        alt="photo"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
